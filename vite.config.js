6ed6ca1e79b2c1a9513d87763f525ad6fedafda9
Takayuki Kanno <jam888@ymail.ne.jp>
2026-01-10 05:12:38 +0900

    add poker page build entry

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  base: '/',
-})
+  build: {
+    rollupOptions: {
+      input: {
+        // SEOトップ
+        main: resolve(__dirname, "index.html"),
+        // ゲーム本体
+        poker: resolve(__dirname, "poker/index.html"),
+        // 日本語トップ（作っている場合）
+        ja: resolve(__dirname, "ja/index.html"),
+      },
+    },
+  },
+});
